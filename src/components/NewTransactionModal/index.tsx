aac3cac67a4f2efb2ccc94241acd62a913881694
Zilla3k <henriquepelanda.web@gmail.com>
2023-01-24 21:45:58 -0300

    create-hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,12 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 import  Modal  from 'react-modal';
-import { TransactionsContext } from '../../TransactionsContext';
 
 import entradasImg from '../../assets/entradas.svg'
 import saidasImg from '../../assets/saidas.svg'
 import closeImg from '../../assets/fecharbtn.svg'
 
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
+import { useTransactions } from '../../hooks/useTransactions';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps){
-  const {createTransaction} = useContext(TransactionsContext)
+  const { createTransaction } = useTransactions();
 
   const [title,setTitle] = useState('')
   const [amount,setAmount] = useState(0)
